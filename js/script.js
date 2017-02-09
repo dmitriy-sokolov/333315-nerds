cdfc9bdeccf38c317b4676eca81262ed67a99a8a
dmitriy-sokolov <sokolovde@mail.ru>
2017-02-09 22:48:59 +0300

    Главная страница
    Добавлено, модальное окно. Окно открывается по нажатию на кнопку.
    Исправлены css стили.

--- js/script.js
+++ js/script.js
@@ -5,12 +5,35 @@
       var value = event.target['value'];
       var sliders = document.querySelectorAll('.slider');
       for(var i=0; i<sliders.length; i++){
-        sliders[i].style.display='none';
+        sliders[i].classList.remove('visible');
       }
-      sliders[value-1].style.display='block'
+      sliders[value-1].classList.add('visible');
     }
 
-    for(var i=0; i<switchers.length; i++){
-      switchers[i].addEventListener('click', changeSlide);
+    if (switchers.length > 0){
+      for(var i=0; i<switchers.length; i++){
+        switchers[i].addEventListener('click', changeSlide);
+      }
     }
+
+    var overlay = document.querySelector('.modal-overlay');
+    var from = document.querySelector('.modal-content');
+    var feedback = document.querySelector('.feedback a');
+    var closeBtn = document.querySelector('.modal-content-close');
+    var loginField = from.querySelector('[name=name]');
+    var doShow = function(event){
+      event.preventDefault();
+      overlay.classList.add("visible");
+      from.classList.add("visible");
+      loginField.focus();
+    };
+
+    var doClose = function(event){
+      event.preventDefault();
+      overlay.classList.remove("visible");
+      from.classList.remove("visible");
+    };
+    feedback.addEventListener("click", doShow)
+    closeBtn.addEventListener("click", doClose);
+    overlay.addEventListener("click", doClose);
 })();
