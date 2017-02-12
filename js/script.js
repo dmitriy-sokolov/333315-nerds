12c4bd104510c2a3ec496e7b8c50bdf646a0f193
dmitriy-sokolov <sokolovde@mail.ru>
2017-02-12 23:23:47 +0300

    Добавление карты + замечания

--- js/script.js
+++ js/script.js
@@ -1,39 +1,35 @@
-(function(){
-    var switchers = document.querySelectorAll('.switch input[type="radio"]');
-
-    var changeSlide  = function(event){
-      var value = event.target['value'];
-      var sliders = document.querySelectorAll('.slider');
-      for(var i=0; i<sliders.length; i++){
-        sliders[i].classList.remove('visible');
-      }
-      sliders[value-1].classList.add('visible');
+(function() {
+  var switchers = document.querySelectorAll('.switch input[type="radio"]');
+  var changeSlide = function(event) {
+    var value = event.target['value'];
+    var sliders = document.querySelectorAll('.slider');
+    for (var i = 0; i < sliders.length; i++) {
+      sliders[i].classList.remove('visible');
     }
-
-    if (switchers.length > 0){
-      for(var i=0; i<switchers.length; i++){
-        switchers[i].addEventListener('click', changeSlide);
-      }
+    sliders[value - 1].classList.add('visible');
+  }
+  if (switchers.length > 0) {
+    for (var i = 0; i < switchers.length; i++) {
+      switchers[i].addEventListener('click', changeSlide);
     }
-
-    var overlay = document.querySelector('.modal-overlay');
-    var from = document.querySelector('.modal-content');
-    var feedback = document.querySelector('.feedback a');
-    var closeBtn = document.querySelector('.modal-content-close');
-    var loginField = from.querySelector('[name=name]');
-    var doShow = function(event){
-      event.preventDefault();
-      overlay.classList.add("visible");
-      from.classList.add("show-modal");
-      loginField.focus();
-    };
-
-    var doClose = function(event){
-      event.preventDefault();
-      overlay.classList.remove("visible");
-      from.classList.remove("show-modal");
-    };
-    feedback.addEventListener("click", doShow)
-    closeBtn.addEventListener("click", doClose);
-    overlay.addEventListener("click", doClose);
+  }
+  var overlay = document.querySelector('.modal-overlay');
+  var from = document.querySelector('.modal-content');
+  var feedback = document.querySelector('.feedback a');
+  var closeBtn = document.querySelector('.modal-content-close');
+  var loginField = from.querySelector('[name=name]');
+  var doShow = function(event) {
+    event.preventDefault();
+    overlay.classList.add("visible");
+    from.classList.add("show-modal");
+    loginField.focus();
+  };
+  var doClose = function(event) {
+    event.preventDefault();
+    overlay.classList.remove("visible");
+    from.classList.remove("show-modal");
+  };
+  feedback.addEventListener("click", doShow)
+  closeBtn.addEventListener("click", doClose);
+  overlay.addEventListener("click", doClose);
 })();
